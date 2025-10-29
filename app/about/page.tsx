35e8cd6e9ec7d42e361375c14e73fe3f43fb8486
Sharon-Kairu <sharonkairu62@gmail.com>
2025-10-29 18:18:13 +0300

    Google Reviews card

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -46,7 +46,7 @@ const Page = () => {
 
       </div>
       <h1 className="text-xl md:text-4xl font-bold mb-4 text-orange-400">The Kena Difference</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-7 p-3 m-2 md:m-6 '>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-7 p-3 m-2 md:m-6 '>
         <div className='bg-gray-900 p-3 flex flex-col gap-4 items-center justify-center rounded-xl'>
             <div className='rounded-full p-3 bg-orange-400 w-16 h-16'>
                 <img src='/meeting.png' alt='Mission' />
@@ -73,14 +73,18 @@ const Page = () => {
                  & its environs. We're not just a school; we're your partners for the long haul..</p>    
         </div>
         <div className='bg-gray-900 p-3 flex flex-col gap-4 items-center justify-center rounded-xl'>
-            <div className='rounded-full p-3 bg-orange-400 w-16 h-16'>
-                <img src='/trust.png' alt='Mission' />
-            </div>
-            <h1 className='text-orange-400 font-bold text-2xl'> Our Promise</h1>
-            <p className='text-white'>We're proud to be known as the best driving school And Computer college in Thika.
-                 Our commitment to affordable pricing and five-star service in google has earned us the best reviews in Thika
-                 & its environs. We're not just a school; we're your partners for the long haul..</p>    
+        <div className='rounded-full p-3 bg-orange-400 w-16 h-16'>
+          <img src='/satisfaction.png' alt='Google Reviews' />
         </div>
+        <h1 className='text-orange-400 font-bold text-2xl'>Google Reviews</h1>
+        <p className='text-white text-center'>
+          Our students have shared their experiences on Google — and the feedback speaks for itself!  
+          With numerous 5-star reviews, we’re proud to be recognized as one of the most trusted 
+          driving schools and computer colleges in Thika.  
+          Your success stories inspire us to keep delivering quality training every day.
+        </p>
+      </div>
+
       </div>
       </div>
   )
