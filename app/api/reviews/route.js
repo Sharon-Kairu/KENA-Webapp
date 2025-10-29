f616f8e4e991393264f51239536a3c9d7e06a911
Sharon-Kairu <sharonkairu62@gmail.com>
2025-10-29 18:07:48 +0300

    Api Key change

--- app/api/reviews/route.js
+++ app/api/reviews/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const placeId = 'ChIJvdj-2Y5OLxgRdS9IxGyfjd8'
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY
+  const apiKey = process.env.GOOGLE_MAPS_API
 
   const res = await fetch(
     `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,reviews,user_ratings_total&key=${apiKey}`
